53e4d62dbba465ae5032803816f6bf8488d51ede
hannahhan153 <hannah.han@biola.edu>
2022-04-19 09:22:49 -0700

    create and test routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,13 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+        .populate({
+            path:'comments',
+            select: '-__v'
+            // minus sign indicates we don't want it to be returned
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
@@ -14,6 +21,11 @@ const pizzaController = {
     // get one pizza by id 
     getPizzaById({ params }, res) {
         Pizza.findOne({_id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbPizzaData => {
             // if no pizza is found, send a 404 error
             if (!dbPizzaData) {
